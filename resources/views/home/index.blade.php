dfb2858ad06289430cf7a63d22f4fcbe210e1d28
Fadliabdill <fadliabdill287@gmail.com>
2024-08-06 15:44:24 +0700

    mengubah title

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,7 +7,7 @@
 <html>
 
 <head>
-    <title>Helios by HTML5 UP</title>
+    <title>Altry</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
